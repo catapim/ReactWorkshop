3ae552112e9b2a69732b3575d37f18d2a0d1fb31
catalina <catalinapimentel@gmail.com>
2019-08-10 10:54:25 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -3,17 +3,47 @@ import { hot } from 'react-hot-loader';
 import './App.scss';
 import TodayList from './components/TodayList';
 import TomorrowList from './components/TomorrowList';
+import ErrorBoundary from './components/ErrorBoundary';
+import Button from './components/Button';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.buttonRefCallback = (elem) => {this.buttonRef = elem;};
+    this.rollToButton = this.rollToButton.bind(this);
+
   }
+
+  rollToButton() {
+    if (this.buttonRef) {
+      this.buttonRef.scrollIntoView({
+        behavior: 'smooth',
+        block:'start',
+      })
+    }
+  }
+
 
   render() {
     return (
       <div className="App" id="app">
-        <TodayList />
-        <TomorrowList />
+
+        <ErrorBoundary>
+          <TodayList title="lista de hoy"></TodayList>
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <TodayList/>
+        </ErrorBoundary>
+        
+        <ErrorBoundary>
+          <TomorrowList />
+        </ErrorBoundary>
+        
+
+        <Button text="Roll to top!!!" buttonRef={this.buttonRefCallback} onClick={this.rollToButton} />
+
+
       </div>
     );
   }
